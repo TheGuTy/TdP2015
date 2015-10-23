b725f65184b6d9f7fb79d59fe54bf56c7cb5475a
GuTy <nggutierrez.45@gmail.com>
2015-10-23 20:12:07 -0300

    javadoc nuevo nuewvo

--- PrototipoBomberman/src/juego/Celda.java
+++ PrototipoBomberman/src/juego/Celda.java
@@ -8,6 +8,10 @@ import personajes.Bomberman;
 import personajes.Enemigo;
 import powerups.PowerUp;
 
+/**
+ * Clase encargada de modelar la celda correspondiente a cada posicion lógica.
+ * @author Asencio Victor, Gutierrez Gabriel, Iurchuk Joaquín
+ */
 public class Celda {
 
 	protected int x;
@@ -18,6 +22,13 @@ public class Celda {
 	protected List<Enemigo> misEnemigos;
 	protected Bomberman miBomberman;
 
+	/**
+	 * Constructor de clase.
+	 * Crea una celda en una posición lógica indicada.
+	 * @param x Posicion lógica horizontal de la celda.
+	 * @param y Posicion lógica vertical de la celda.
+	 * @param t Referencia al tablero de juego.
+	 */
 	public Celda(int x, int y, Tablero t) {
 
 		this.x = x;
@@ -28,36 +39,59 @@ public class Celda {
 		misEnemigos = new LinkedList<Enemigo>();
 		miBomberman = null;
 	}
-
+	
+	/**
+	 * Metodo que le indica al estado actual de la celda que mueva
+	 * al bomberman en una dirección indicada.
+	 * @param bomberman Referencia a Bomberman.
+	 * @param dir Dirección de movimiento.
+	 */
 	public void avanzar(Bomberman bomberman, int dir) {
 		miEstado.avanzar(bomberman, this, dir);
 	}
 
+	/**
+	 * Metodo que le indica al estado actual de la celda que mueva
+	 * al enemigo en una dirección indicada.
+	 * @param enemigo Referencia al enemigo.
+	 * @param dir Dirección de movimiento.
+	 */
 	public void avanzar(Enemigo enemigo, int dir) {
 		miEstado.avanzar(enemigo, this, dir);
 	}
-
+	
+	/**
+	 * Agrega un power un a esta celda.
+	 * @param powerup PowerUp a colocar.
+	 */
 	public void setPowerUp(PowerUp powerup) {
 
 	}
-
+	
+	/**
+	 * Retorna el PowerUp ubicado en esta celda, si no hay power up retorna null.
+	 * @return PowerUp almacenado en la celda.
+	 */
 	public PowerUp getPowerUp() {
-
 		return miPowerUp;
 	}
-
+	
+	/**
+	 * Setter para cambiar el estado actual de la celda.
+	 * @param e Estado al cual pasará la celda.
+	 */
 	public void setEstado(EstadoCelda e) {
-
 		miEstado = e;
 		miTablero.restaurarCelda(this);
 		miTablero.agregarEnGUI(miEstado.getGrafico().getLabel());
 	}
-
+	
+	/**
+	 * Elimina todos los enemigos ubicados en ésta celda.
+	 */
 	public void destruirEnemigos() {
 		
 		List<Enemigo> aMatar = new LinkedList<Enemigo>();
-		miTablero.agregarEnGUI(miEstado.getGrafico().getLabel());
-		miTablero.agregarEnGUI(miEstado.getGrafico().getLabel());
 		
 		for (Enemigo e : misEnemigos){
 			aMatar.add(e);
@@ -69,48 +103,85 @@ public class Celda {
 		
 		miTablero.getJuego().matarEnemigo(this);
 	}
-
+	
+	/**
+	 * Incrementa el puntaje del juego en una cantidad indicada.
+	 * @param p Cantidad de puntaje a incrementar.
+	 */
 	public void aumentarPuntaje(int p) {
 
 		miTablero.aumentarPuntaje(p);
 	}
 
+	/**
+	 * Agrega un enemigo a la lista de enemigos perteneciente a la celda.
+	 * @param enemigo Enemigo a agregar a la lista.
+	 */
 	public void agregarEnemigo(Enemigo enemigo) {
 		misEnemigos.add(enemigo);
 	}
 
+	/**
+	 * Elimina un enemigo de la lista de enemigos perteneciente a la celda.
+	 * @param enemigo Enemigo a eliminar de la lista.
+	 */
 	public void eliminarEnemigo(Enemigo enemigo) {
 		misEnemigos.remove(enemigo);
 	}
 
+	/**
+	 * Agrega a Bomberman a ésta celda.
+	 * @param b Referencia a Bomberman.
+	 */
 	public void agregarBomberman(Bomberman b) {
 		miBomberman = b;
 	}
 
+	/**
+	 * Quita a Bomberman de ésta celda.
+	 */
 	public void eliminarBomberman() {
 		miBomberman = null;
 	}
 
+	/**
+	 * Retorna la posición lógica horizontal de la celda.
+	 * @return Posición horizontal.
+	 */
 	public int getX() {
-
 		return x;
 	}
-
+	
+	/**
+	 * Retorna la posición lógica vertical de la celda.
+	 * @return Posición vertical.
+	 */
 	public int getY() {
 
 		return y;
 	}
 
+	/**
+	 * Chequea si hay enemigos actualmente en ésta celda.
+	 * @return true si hay enemigos, false en caso contrario.
+	 */
 	public boolean hayEnemigos() {
 
 		return !misEnemigos.isEmpty();
 	}
 
+	/**
+	 * Chequea si el Bomberman se encuentra en ésta celda.
+	 * @return true si el Bomberman está en ésta celda, false en caso contrario.
+	 */
 	public boolean hayBomberman() {
 
 		return miBomberman != null;
 	}
 
+	/**
+	 * Método que le indica a la lógica del juego que el Bomberman ha muerto.
+	 */
 	public void matarBomberman() {
 
 		if (miBomberman != null){
@@ -118,11 +189,19 @@ public class Celda {
 		}
 		// TODO Notificar al juego que murio bomberman. Desde aca el unico camino es usar el tablero y desde ahi obtener la referencia al juego 
 	}
-
+	
+	/**
+	 * Método que retorna el estado actual de la celda.
+	 * @return Estado actual de la celda.
+	 */
 	public EstadoCelda getEstado() {
 		return miEstado;
 	}
 
+	/**
+	 * Metodo que le indica al estado actual de la celda que 
+	 * una bomba afectó a ésta celda y hay que modificar su contenido.
+	 */
 	public void detonar() {
 		miEstado.destruir(this);
 	}
